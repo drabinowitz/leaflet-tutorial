90f18bf8a33ea38e1ce1f30c84aed0a5cb7a032e
dmitri <dmitri@vistarmedia.com>
2015-09-07 19:42:52 -0400

    Add geojson features

--- index.js
+++ index.js
@@ -2,13 +2,82 @@ require('leaflet/dist/leaflet.css');
 var L = require('leaflet');
 L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images';
 var appConfig = require('./config.js');
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 2);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
   maxZoom: 18,
   id: appConfig.PROJECT_ID,
   accessToken: appConfig.ACCESS_TOKEN
 }).addTo(map);
+
+var states = [{
+  "type": "Feature",
+  "properties": {"party": "Republican"},
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [[
+      [-104.05, 48.99],
+      [-97.22,  48.98],
+      [-96.58,  45.94],
+      [-104.03, 45.94],
+      [-104.05, 48.99]
+    ]]
+  }
+}, {
+  "type": "Feature",
+  "properties": {"party": "Democrat"},
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [[
+      [-109.05, 41.00],
+      [-102.06, 40.99],
+      [-102.03, 36.99],
+      [-109.04, 36.99],
+      [-109.05, 41.00]
+    ]]
+  }
+}];
+
+L.geoJson(states, {
+  style: function(feature) {
+    switch (feature.properties.party) {
+      case 'Republican': return {color: "#ff0000"};
+      case 'Democrat':   return {color: "#0000ff"};
+    }
+  }
+}).addTo(map);
+
+var geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.8
+};
+
+var someGeojsonFeature = {
+  "type": "Feature",
+  "properties": {
+    "name": "Coors Field",
+    "amenity": "Baseball Stadium",
+    "popupContent": "This is where the Rockies play!"
+  },
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-104.99404, 39.75621]
+  }
+};
+
+L.geoJson(someGeojsonFeature, {
+  pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng, geojsonMarkerOptions);
+  },
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup(feature.properties.popupContent);
+  }
+}).addTo(map);
+
 var marker = L.marker([51.5, -0.09]).addTo(map);
 var circle = L.circle([51.508, -0.11], 500, {
   color: 'red',
