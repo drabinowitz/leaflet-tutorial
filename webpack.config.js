03ad8a4efbf496cae19b3d3e585538c4a5a99f3e
dmitri <dmitri@vistarmedia.com>
2015-09-07 17:48:22 -0400

    Requiring css

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,11 @@ module.exports = {
     path: __dirname + '/bin/',
     filename: "bundle.js",
     publicPath: "/bin/"
-  }
+  },
+  module: {
+    loaders: [
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+   ]
+ }
 };
