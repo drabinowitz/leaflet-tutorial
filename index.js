3daf1c2ad0fea5b5b1573e205980a098af22a39b
dmitri <dmitri@vistarmedia.com>
2015-09-07 21:45:47 -0400

    Implement us states

--- index.js
+++ index.js
@@ -1,109 +1,107 @@
 require('leaflet/dist/leaflet.css');
 var L = require('leaflet');
 L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images';
-var appConfig = require('./config.js');
-var map = L.map('map').setView([51.505, -0.09], 2);
+var map = L.map('map').setView([37.8, -96], 4);
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  id: 'mapbox.light',
+  accessToken: require('./config.js').ACCESS_TOKEN,
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  maxZoom: 18,
-  id: appConfig.PROJECT_ID,
-  accessToken: appConfig.ACCESS_TOKEN
 }).addTo(map);
 
-var states = [{
-  "type": "Feature",
-  "properties": {"party": "Republican"},
-  "geometry": {
-    "type": "Polygon",
-    "coordinates": [[
-      [-104.05, 48.99],
-      [-97.22,  48.98],
-      [-96.58,  45.94],
-      [-104.03, 45.94],
-      [-104.05, 48.99]
-    ]]
-  }
-}, {
-  "type": "Feature",
-  "properties": {"party": "Democrat"},
-  "geometry": {
-    "type": "Polygon",
-    "coordinates": [[
-      [-109.05, 41.00],
-      [-102.06, 40.99],
-      [-102.03, 36.99],
-      [-109.04, 36.99],
-      [-109.05, 41.00]
-    ]]
-  }
-}];
-
-L.geoJson(states, {
-  style: function(feature) {
-    switch (feature.properties.party) {
-      case 'Republican': return {color: "#ff0000"};
-      case 'Democrat':   return {color: "#0000ff"};
-    }
-  }
-}).addTo(map);
-
-var geojsonMarkerOptions = {
-  radius: 8,
-  fillColor: "#ff7800",
-  color: "#000",
-  weight: 1,
-  opacity: 1,
-  fillOpacity: 0.8
-};
-
-var someGeojsonFeature = {
-  "type": "Feature",
-  "properties": {
-    "name": "Coors Field",
-    "amenity": "Baseball Stadium",
-    "popupContent": "This is where the Rockies play!"
-  },
-  "geometry": {
-    "type": "Point",
-    "coordinates": [-104.99404, 39.75621]
-  }
-};
-
-L.geoJson(someGeojsonFeature, {
-  pointToLayer: function (feature, latlng) {
-    return L.circleMarker(latlng, geojsonMarkerOptions);
-  },
-  onEachFeature: function (feature, layer) {
-    layer.bindPopup(feature.properties.popupContent);
-  }
-}).addTo(map);
-
-var marker = L.marker([51.5, -0.09]).addTo(map);
-var circle = L.circle([51.508, -0.11], 500, {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5
-}).addTo(map);
-var polygon = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
-]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
-var popup = L.popup()
-  .setLatLng([51.5, -0.09])
-  .setContent("I am a standalone popup.")
-  .openOn(map);
-
-var popup = L.popup();
-
-function onMapClick(e) {
-  popup
-    .setLatLng(e.latlng)
-    .setContent("You clicked the map at " + e.latlng.toString())
-    .openOn(map);
+function getColor(d) {
+  return d > 1000 ? '#800026' :
+         d > 500  ? '#BD0026' :
+         d > 200  ? '#E31A1C' :
+         d > 100  ? '#FC4E2A' :
+         d > 50   ? '#FD8D3C' :
+         d > 20   ? '#FEB24C' :
+         d > 10   ? '#FED976' :
+                    '#FFEDA0';
 }
 
-map.on('click', onMapClick);
+function style(feature) {
+  return {
+    fillColor: getColor(feature.properties.density),
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7
+  };
+}
+
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+    weight: 5,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera) {
+    layer.bringToFront();
+  }
+  info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+  info.update();
+}
+
+function zoomToFeature(e) {
+  map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: zoomToFeature
+  });
+}
+
+var geojson = L.geoJson(require('./us-states.js'), {
+  style: style,
+  onEachFeature: onEachFeature
+}).addTo(map);
+
+var info = L.control();
+
+info.onAdd = function (map) {
+  this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+  this.update();
+  return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+  this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+      '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+      : 'Hover over a state');
+};
+
+info.addTo(map);
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+  var div = L.DomUtil.create('div', 'info legend'),
+    grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+    labels = [];
+
+  // loop through our density intervals and generate a label with a colored square for each interval
+  for (var i = 0; i < grades.length; i++) {
+    div.innerHTML +=
+      '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
+
+  return div;
+};
+
+legend.addTo(map);
